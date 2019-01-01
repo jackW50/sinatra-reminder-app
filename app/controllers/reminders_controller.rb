5252dea0b707f9ecb40be3456197a1ea88a68d25
John Werenski <jtwerenski@gmail.com>
2019-01-01 17:28:59 +0000

    add routes to reminders_controller

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,2 +1,31 @@
 class RemindersController < ApplicationController
+  
+  get "/reminders" do
+    erb :"/reminders/reminders"
+  end 
+  
+  get "/reminders/new" do 
+    erb :"reminders/new"
+  end 
+  
+  post "/reminders" do 
+    redirect "/reminders/#{reminder.id}"
+  end 
+  
+  get "/reminders/:id" do 
+    erb :"/reminders/show"
+  end 
+  
+  get "/reminders/:id/edit" do 
+    erb :"/reminders/edit"
+  end 
+  
+  patch "/reminders/:id" do 
+    redirect "/reminders/#{reminder.id}"
+  end 
+  
+  delete "/reminders/:id/delete" do 
+    redirect "/users/#{user.id}"
+  end 
+  
 end 
