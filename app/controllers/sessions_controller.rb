d48e10e4c11ec752d7def4e83da2d9dd1d1592e3
John Werenski <jtwerenski@gmail.com>
2019-01-01 17:07:12 +0000

    add routes to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  
+  get "/login" do 
+    erb :"/sessions/login"
+  end 
+  
+  get "/signup" do 
+    erb:"/sessions/signup"
+  end 
+  
+  post "/login" do 
+    redirect "/users/:id"
+  end 
+  
+  post "/signup" do 
+    redirect "/users/:id"
+  end 
+  
+  get "/logout" do 
+    redirect "/"
+  end 
 end 
