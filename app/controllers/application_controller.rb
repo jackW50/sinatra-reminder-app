ede8f4840dc83ff9d9db29081c81e0bf38515f8c
John Werenski <jtwerenski@gmail.com>
2019-01-01 21:15:58 +0000

    add helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,10 @@ class ApplicationController < Sinatra::Base
     def current_user 
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end 
+    
+    def user_permission?(user)
+      user.id == current_user.id 
+    end 
   end 
 
 end
