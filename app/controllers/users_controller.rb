e89b07f09b1cc575b9499996547eb540c25c7fa9
John Werenski <jtwerenski@gmail.com>
2019-01-01 17:18:08 +0000

    add routes to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController 
+  
+  get "/users/:id" do 
+    erb :"/users/show"
+  end 
+  
+  get "/users/:id/edit" do 
+    erb :"/users/edit"
+  end 
+  
+  patch "/users/:id" do 
+    redirect "/users/#{user.id}"
+  end 
+  
+  delete "/users/:id/delete" do 
+    session.clear
+    redirect "/"
+  end 
+  
 end 
